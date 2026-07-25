85aad51d8448b761fd3fc9afd9ad8d436c4fd24a
Shail Mehta <shailmehta25@gmail.com>
2026-07-25 12:27:51 +0530

    remove heading

--- packages/components/src/scrollable/hook.ts
+++ packages/components/src/scrollable/hook.ts
@@ -1,11 +1,4 @@
-/**
- * External dependencies
- */
 import clsx from 'clsx';
-
-/**
- * Internal dependencies
- */
 import type { WordPressComponentProps } from '../context';
 import { useContextSystem } from '../context';
 import type { ScrollableProps } from './types';
